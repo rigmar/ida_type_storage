18144d3461977082d2f3e71a9d6217c70f3e7b11
Rigmar <rigmar@yandex.ru>
2015-12-09 07:53:48 +0400

    fixes added "Resolve depends" checkbox

--- IdaTypeStringParser.py
+++ IdaTypeStringParser.py
@@ -26,9 +26,60 @@ elif sys.platform == "darwin":
 
 wrapperTypeString = '\x0d\x01\x01'
 
+class qtype(Structure):
+    _fields_ = [("ptr",ctypes.c_char_p),("cur_size",ctypes.c_int),("max_size",ctypes.c_int)]
+
+
 ############################################################
 # Specifying function types for a few IDA SDK functions to keep the
 # pointer-to-pointer args clear.
+
+c_serialize_tinfo = g_dll.serialize_tinfo
+c_serialize_tinfo.argtypes = [
+    ctypes.POINTER(qtype),
+    ctypes.POINTER(qtype),
+    ctypes.POINTER(qtype),
+    ctypes.POINTER(ctypes.c_ulong),
+    ctypes.c_int
+]
+
+c_new_til = g_dll.new_til
+c_new_til.argtyped = [
+    c_char_p,
+    c_char_p
+]
+c_new_til.restype = c_void_p
+
+c_parse_decl2 = g_dll.parse_decl2
+parse_decl2.argtypes = [
+    c_void_p,
+    c_char_p,
+    ctypes.POINTER(qtype),
+    ctypes.POINTER(ctypes.c_ulong),
+    ctypes.c_int
+]
+
+c_deserialize_tinfo = g_dll.deserialize_tinfo
+c_deserialize_tinfo.argtypes = [
+    ctypes.POINTER(ctypes.c_ulong),
+    ctypes.c_void_p,
+    ctypes.POINTER(ctypes.c_char_p),
+    ctypes.POINTER(ctypes.c_char_p),
+    ctypes.POINTER(ctypes.c_char_p)
+]
+
+c_print_tinfo = g_dll.print_tinfo
+c_print_tinfo.argtypes = [
+    ctypes.POINTER(qtype),
+    ctypes.c_char_p,
+    ctypes.c_int,
+    ctypes.c_int,
+    ctypes.c_int,
+    ctypes.POINTER(ctypes.c_ulong),
+    ctypes.c_char_p,
+    ctypes.c_char_p
+]
+
 get_named_type = g_dll.get_named_type
 get_named_type.argtypes = [
     ctypes.c_void_p,                                #const til_t *ti,
@@ -1005,7 +1056,8 @@ class LocalType(object):
 
 
     def find_type_by_name(self, name):
-        ordinal = get_type_ordinal(ti,name)
+        my_ti = cvar.idati
+        ordinal = get_type_ordinal(my_ti,name)
 
     def GetTypeString(self):
         ti = idaapi.cvar.idati
